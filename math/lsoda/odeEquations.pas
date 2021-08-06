eaaff893bc2e963476a2729253e9ebd1853a25e9
bartjuw <barthj@uw.edu>
2021-08-06 13:02:30 -0700

    Remove commented out code

--- math/lsoda/odeEquations.pas
+++ math/lsoda/odeEquations.pas
@@ -6,26 +6,22 @@ uses SysUtils, uVector, Web;
 
 type
  TODE_eqs = class(TObject)
- //  class function ComputeODEs (t: double; y : TVector; p: array of double):TVector;
  class function ComputeODEs (t: double; y, p: array of double):TVector;
  end;
 
 
 implementation
- // return dy vector
+
 class function TODE_eqs.ComputeODEs (t : double; y, p:array of double):TVector;
 begin
   Result:= TVector.create(Length(y));
- // Result[1] := -0.04*y[1] + 10000*y[2]*y[3];
- // Result[3] := 3E7*y[2]*y[2];
- // Result[2] := -Result[1] - Result[3];
-
   Result[1] := p[0]*y[0] + p[1]*y[1]*y[2];
   Result[3] := p[2]*y[1]*y[1];
   Result[2] := -Result[1] - Result[3];
 // console.log(' ComputeODES, y[0], [1], [2]: ', y[0],', ', y[1],', ', y[2]);
 
 end;
+
 
 
 end.
