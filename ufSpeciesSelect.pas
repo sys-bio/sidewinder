68be2156ac1969b8a5ce6c86d2fe64f7a9f9546f
bartjuw <barthj@uw.edu>
2021-09-15 15:45:44 -0700

    remove unneeded unit file references

--- ufSpeciesSelect.pas
+++ ufSpeciesSelect.pas
@@ -7,8 +7,7 @@ interface
 uses
   System.SysUtils, System.Classes, JS, Web, WEBLib.Graphics, WEBLib.Controls,
   WEBLib.Forms, WEBLib.Dialogs, Vcl.Controls, WEBLib.ExtCtrls, Vcl.StdCtrls,
-  WEBLib.StdCtrls, Types, VCL.TMSFNCTypes, VCL.TMSFNCUtils, VCL.TMSFNCGraphics, VCL.TMSFNCGraphicsTypes; {VCL.TMSFNCCustomControl, VCL.TMSFNCHTMLImageContainer,}
-  {VCL.TMSFNCCheckBox;}
+  WEBLib.StdCtrls, Types, VCL.TMSFNCTypes, VCL.TMSFNCUtils, VCL.TMSFNCGraphics, VCL.TMSFNCGraphicsTypes;
 
 type
   TSpeciesSWForm = class(TWebForm)
