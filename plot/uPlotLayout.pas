5d38d80aa7cf998022d7443a7251d3a2f461ebea
bartjuw <barthj@uw.edu>
2021-09-22 08:50:17 -0700

    simplify plot layout and update with new panel container

--- plot/uPlotLayout.pas
+++ plot/uPlotLayout.pas
@@ -5,7 +5,7 @@ uses System.SysUtils, System.Classes, System.Generics.Collections,
 JS, Web, Vcl.Controls, WEBLib.ExtCtrls, WEBLib.Dialogs, uPlotPanel ;
 
 const DEFAULT_NUMB_PLOTS = 3; PLOT_WIDTH_OVERLAP = 30;
-var panelW, panelH, paintBoxW, paintBoxH, currPlot: integer;
+var panelW, panelH,{ paintBoxW, paintBoxH,} currPlot: integer;
 
 procedure configPbPlot(plotNumber, totalPlots, plotPanelWidth : integer; plotPanelHeight: integer;
           plotPanelList: TList<TPlotPanel>);
@@ -28,14 +28,15 @@ begin
       plotPanelList[totalPlots-1].plotWPanel.Height := 200;//round(panelH/3);
       plotPanelList[totalPlots-1].plotPB.Height := plotPanelList[totalPlots-1].plotWPanel.Height - 50;
      end;
-
-   plotPanelList[totalPlots-1].plotWPanel.Width := panelW - PLOT_WIDTH_OVERLAP; // This must be a bg, without it the slider panel overlaps plot
+                                                   // ISSUE: plotLegend has not created yet ..,..
+   //plotPanelList[totalPlots-1].plotWPanel.Width := panelW - PLOT_WIDTH_OVERLAP; // This must be a bg, without it the slider panel overlaps plot
+   plotPanelList[totalPlots-1].plotWPanel.Width := plotPanelList[totalPlots-1].getParentContainer.width - plotPanelList[totalPlots-1].getPlotLegendPB().Width;
    plotPanelList[totalPlots-1].plotWPanel.Left := 15; //10 gap between the network canvas and plot
    plotPanelList[totalPlots-1].plotWPanel.Top := 3 + plotPanelList[totalPlots-1].plotWPanel.Height*(plotNumber-1); // change to variable value based on number of existing plots.
    plotPanelList[totalPlots-1].plotWPanel.visible := true;
    plotPanelList[totalPlots-1].plotWPanel.invalidate;
 
-   plotPanelList[totalPlots-1].plotPB.Width := plotPanelList[totalPlots-1].plotWPanel.Width - 80; // This must be a bg, without it the slider panel overlaps plot
+   plotPanelList[totalPlots-1].plotPB.Width := plotPanelList[totalPlots-1].plotWPanel.Width {- 80}; // This must be a bg, without it the slider panel overlaps plot
    plotPanelList[totalPlots-1].plotPB.Left := 5; // gap between the plotPanel canvas and plot
    plotPanelList[totalPlots-1].plotPB.Top := 5; // change to variable value based on number of existing plots.
    plotPanelList[totalPlots-1].plotPB.visible := true;
